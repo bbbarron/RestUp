05b6834c3e5a223da911b143dce0a8eb4857bccd
Barry Barron <barrybbarron@gmail.com>
2022-08-07 14:45:41 -0700

    Initial Commit

---
+++ RestUp/RestUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestUpApp.swift
+//  RestUp
+//
+//  Created by Barry Barron on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RestUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
