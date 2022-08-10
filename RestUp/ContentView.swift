e8fb7915817430a4f218233152e4f4b2e52a1cd1
Barry Barron <barrybbarron@gmail.com>
2022-08-09 22:06:06 -0700

    Final project with challenges complete. consider making suggested bedtime in Blue color and larger font. Also color the background

--- RestUp/ContentView.swift
+++ RestUp/ContentView.swift
@@ -13,59 +13,14 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
 
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 6
         components.minute = 30
         return Calendar.current.date(from: components) ?? Date.now
     }
-
-    var body: some View {
-        NavigationView {
-            Form {
-                Section("Wake Up Time"){
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                }
-                Section("Sleep Amount") {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                }
-                Section("Coffee Intake") {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                }
-                }
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
-        }
-    }
-
-    func calculateBedtime() {
+    
+    var sleepResults: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -77,15 +32,39 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
 
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return "Your ideal bedtime is.. " + sleepTime.formatted(date:.omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "There was an error"
         }
-
-        showingAlert = true
     }
+   
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("Wake Up Time"){
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                
+                }
+                Section("Sleep Amount") {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                }
+                Section("Daily Coffee Intake") {
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<11) {
+                            Text(String($0))
+                        }
+                    }
+                }
+                
+                Text(sleepResults)
+                    .font(.title3)
+            }
+            .navigationTitle("BetterRest")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -93,4 +72,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
